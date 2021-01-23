a98b483bf9ebcd56c37d388b82a90b5e63a0aa13
Kai Engwall <kengwall8@gmail.com>
2021-01-23 14:49:56 -0600

    second commit

---
+++ kaiengwall/src/AboutMe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+
+export const AboutMe = () =>(
+    <div className = "aboutMe">
+          <h2>About Me</h2>
+          <p>
+            Hi! My name is Kai Engwall and I'm a sophomore studying Computer Science at Stevens Institute of Technology in Hoboken, NJ.
+            Some languages that I use or have used in the past include C++, Java, Python, JavaScript, and C#.
+            
+          </p>
+        </div>
+)
--- kaiengwall/src/App.js
+++ kaiengwall/src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import {NavBar} from './NavBar';
-import {Border} from './Border';  
+import {Border} from './Border'; 
+import {AboutMe} from './AboutMe';
 function App() {
   return (
     <div>
       <NavBar/>
       <div className = "main">
         <div >
+          <h1 className = "construct">WEBSITE UNDER CONSTRUCTION</h1>
           <h1 className = "welcomeTitle">Welcome to my Website</h1>
         </div>
-        
+      <AboutMe/>
       </div>
       <Border/>
     </div>
